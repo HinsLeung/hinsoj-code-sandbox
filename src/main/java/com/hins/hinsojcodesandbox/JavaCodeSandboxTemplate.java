1e8616d9b6beabe252669457436c937de7567853
HinsLeung <2597639004@qq.com>
2024-08-08 00:38:58 +0800

    8.7.2

---
+++ src/main/java/com/hins/hinsojcodesandbox/JavaCodeSandboxTemplate.java
@@ -0,0 +1,198 @@
+package com.hins.hinsojcodesandbox;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.dfa.FoundWord;
+import cn.hutool.dfa.WordTree;
+import com.hins.hinsojcodesandbox.model.ExecuteCodeRequest;
+import com.hins.hinsojcodesandbox.model.ExecuteCodeResponse;
+import com.hins.hinsojcodesandbox.model.ExecuteMessage;
+import com.hins.hinsojcodesandbox.model.JudgeInfo;
+import com.hins.hinsojcodesandbox.security.DefaultSecurityManager;
+import com.hins.hinsojcodesandbox.utils.ProcessUtils;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+public abstract class JavaCodeSandboxTemplate implements CodeSandbox{
+
+    @Override
+    public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
+        return null;
+    }
+
+    private static final String GLOBAL_COOE_DIR_NAME = "tmpCode";
+
+    private static final String GLOBAL_JAVA_CLASS_NAME = "Main.java";
+
+    private static final long TIME_OUT = 5000L;
+
+    /**
+     * 1.把用户的代码保存为文件
+     * @param code
+     * @return
+     */
+    public File saveCodeToFile(String code) {
+        //1.把用户的代码保存文件
+
+        String userDir = System.getProperty("user.dir");
+        String globalCodePathName = userDir + File.separator + GLOBAL_COOE_DIR_NAME;
+        //判断全局代码目录是否存在，没有则新建
+        if (!FileUtil.exist(globalCodePathName)) {
+            FileUtil.mkdir(globalCodePathName);
+        }
+        //把用户的代码隔离存放
+        String userCodeParentPath = globalCodePathName + File.separator + UUID.randomUUID();
+        String userCodePath = userCodeParentPath + File.separator + GLOBAL_JAVA_CLASS_NAME;
+        File userCodeFide = FileUtil.writeString(code, userCodePath, StandardCharsets.UTF_8);
+
+        return userCodeFide;
+    }
+
+    /**
+     * 编译代码，得到class文件
+     * @param userCodeFide
+     * @return
+     */
+    public ExecuteMessage compileFile(File userCodeFide) {
+        String compileCmd = String.format("javac -encoding utf-8 %s", userCodeFide.getAbsolutePath());
+        try {
+            Process compileProcess = Runtime.getRuntime().exec(compileCmd);
+            ExecuteMessage executeMessage = ProcessUtils.runProcessAndGetMessage(compileProcess, "编译");
+            if (executeMessage.getExitValue() != 0){
+                throw new RuntimeException("编译错误");
+            }
+            return executeMessage;
+        } catch (Exception e) {
+//            return getErrorResponse(e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 执行文件。获得执行结果类
+     * @param userCodeFile
+     * @param inputList
+     * @return
+     */
+    public List<ExecuteMessage> runFile(File userCodeFile, List<String> inputList){
+        String userDir = System.getProperty("user.dir");
+        String globalCodePathName = userDir + File.separator + GLOBAL_COOE_DIR_NAME;
+        String userCodeParentPath = globalCodePathName + File.separator + UUID.randomUUID();
+        List<ExecuteMessage> executeMessageList = new ArrayList<>();
+        for (String inputArgs : inputList) {
+            //限制内存是256MB(最大堆空间大小)
+//            String runCmd = String.format("java -Xmx256m -Dfile.encoding=UTF-8 -cp %s Main %s", userCodeParentPath, inputArgs);
+            String runCmd = String.format("java -Xmx256m -Dfile.encoding=UTF-8 -cp %s;%s -Djava.security.manager=MySecurityManage Main %s", userCodeParentPath, inputArgs);
+
+            try {
+                Process runProcess = Runtime.getRuntime().exec(runCmd);
+                //守护线程限制时间
+                new Thread(() -> {
+                    try {
+                        Thread.sleep(TIME_OUT);
+                        System.out.println("超时了，中断");
+                        runProcess.destroy();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).start();
+
+                ExecuteMessage executeMessage = ProcessUtils.runProcessAndGetMessage(runProcess, "运行");
+//                ExecuteMessage executeMessage = ProcessUtils.runInteractProcessAndGetMessage(runProcess, "运行",inputArgs);
+                System.out.println(executeMessage);
+                executeMessageList.add(executeMessage);
+            } catch (Exception e) {
+                throw new  RuntimeException("程序执行异常",e);
+            }
+        }
+        return executeMessageList;
+    }
+
+    @Override
+    public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
+
+        System.setSecurityManager(new DefaultSecurityManager());
+
+        List<String> inputList = executeCodeRequest.getInputList();
+        String code = executeCodeRequest.getCode();
+        String language = executeCodeRequest.getLanguage();
+
+        //1.把用户的代码保存文件
+        File userCodeFile = saveCodeToFile(code);
+
+
+        //2.编译代码，得到class文件
+        ExecuteMessage compileFileExecuteMessage = compileFile(userCodeFile);
+        System.out.println(compileFileExecuteMessage);
+
+
+        //3.执行代码、得到输出结果
+
+        List<ExecuteMessage> executeMessageList = runFile(userCodeFile, inputList);
+
+        //4.手机整理输出结果
+        ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
+        List<String> outputList = new ArrayList<>();
+        //取用时最大值，便于判断是否超时
+        long maxTime = 0;
+        for (ExecuteMessage executeMessage : executeMessageList) {
+            String errorMessage = executeMessage.getErrorMessage();
+            if (StrUtil.isNotBlank(errorMessage)) {
+                executeCodeResponse.setMessage(errorMessage);
+                //用户提交的代码在执行中存在错误
+                executeCodeResponse.setStatus(3);
+                break;
+            }
+            outputList.add(executeMessage.getMessage());
+            Long time = executeMessage.getTime();
+            if (time != null) {
+                maxTime = Math.max(maxTime, time);
+            }
+        }
+        //正常运行完成
+        if (outputList.size() == executeMessageList.size()) {
+            executeCodeResponse.setStatus(1);
+        }
+        executeCodeResponse.setOutputList(outputList);
+
+        JudgeInfo judgeInfo = new JudgeInfo();
+        judgeInfo.setTime(maxTime);
+
+        executeCodeResponse.setJudgeInfo(judgeInfo);
+
+        //5.文件清理
+        if (userCodeFide.getParentFile() != null) {
+            boolean del = FileUtil.del(userCodeParentPath);
+            System.out.println("删除" + (del ? "成功" : "失败"));
+        }
+
+        //6.错误处理 提升程序健壮性
+
+
+        return executeCodeResponse;
+    }
+
+    /**
+     * 获取错误响应
+     *
+     * @param e
+     * @return
+     */
+    private ExecuteCodeResponse getErrorResponse(Throwable e) {
+        ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
+        executeCodeResponse.setOutputList(new ArrayList<>());
+        executeCodeResponse.setMessage(e.getMessage());
+        //表示代码沙箱错误
+        executeCodeResponse.setStatus(2);
+        executeCodeResponse.setJudgeInfo(new JudgeInfo());
+        return executeCodeResponse;
+
+    }
+
+}
--- src/main/java/com/hins/hinsojcodesandbox/security/DefaultSecurityManager.java
+++ src/main/java/com/hins/hinsojcodesandbox/security/DefaultSecurityManager.java
@@ -10,8 +10,8 @@ public class DefaultSecurityManager extends SecurityManager{
     // 检查所有的权限
     @Override
     public void checkPermission(Permission perm) {
-        System.out.println("默认不做任何权限限制");
-        System.out.println(perm);
+//        System.out.println("默认不做任何权限限制");
+//        System.out.println(perm);
 //        super.checkPermission(perm);
     }
 
