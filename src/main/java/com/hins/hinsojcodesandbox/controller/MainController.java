03e78a90c48dca56db2c22e2aea7b3d074da86fd
HinsLeung <2597639004@qq.com>
2024-08-09 00:20:54 +0800

    8.8.2

--- src/main/java/com/hins/hinsojcodesandbox/controller/MainController.java
+++ src/main/java/com/hins/hinsojcodesandbox/controller/MainController.java
@@ -9,9 +9,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController("/")
 public class MainController {
+
+    public static final String AUTH_REQUEST_HEADER = "auth";
+
+    private static final String AUTH_REQUEST_SECRET = "secretKey";
 
     @Resource
     private JavaNativeCodeSandbox javaNativeCodeSandbox;
@@ -27,7 +33,15 @@ public class MainController {
      * @return
      */
     @PostMapping("/executeCode")
-    ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest){
+    ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest, HttpServletRequest request,
+                                    HttpServletResponse response){
+        // 基本的认证
+        String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!AUTH_REQUEST_SECRET.equals(authHeader)){
+            response.setStatus(403);
+            return null;
+        }
+
         if (executeCodeRequest == null){
             throw new RuntimeException("请求参数为空");
         }
